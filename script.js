b489b825502cec4df34527cc61f7aff5e0cc3212
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-06 22:07:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent the form from submitting (since we handle it with JavaScript)
+    event.preventDefault();
 
+    // Get the form element
+    const form = document.getElementById('myForm');
+
+    // Get the values of First Name and Last Name inputs
+    const firstName = form.elements['firstName'].value;
+    const lastName = form.elements['lastName'].value;
+
+    // Display the values using alert
+    alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 }
+
+// Attach event listener to the form's submit event
+const form = document.getElementById('myForm');
+form.addEventListener('submit', getFormvalue);
